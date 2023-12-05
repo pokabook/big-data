d2199bb53166fd7e5048f31d672528f7c79a0731
poka <khs0826@dsm.hs.kr>
2023-12-05 20:38:01 +0900

    add data

--- graph/graph.go
+++ graph/graph.go
@@ -193,7 +193,7 @@ func GenerateGraph(topTechstacks []utils.TechstackCount, techstacks []utils.Tech
 
 	page := components.NewPage()
 	page.PageTitle = "기업별 사용 기술 스택 분석"
-	page.AddCharts(generateWordCloud(topTechstacks))
+	page.AddCharts(generateWordCloud(techstacks))
 	page.AddCharts(generateBar(topTechstacks))
 
 	for category, techStacks := range categoryData {
