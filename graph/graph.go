8408ad80ad86b04c33f13e830e0425244548d50e
poka <khs0826@dsm.hs.kr>
2023-12-05 20:21:16 +0900

    add '('

--- graph/graph.go
+++ graph/graph.go
@@ -163,6 +163,26 @@ func generatePie(category string, techStacks []utils.TechstackCount) *charts.Pie
 	return pie
 }
 
+func generateWordCloud(techstacks []utils.TechstackCount) *charts.WordCloud {
+
+	wordCloud := charts.NewWordCloud()
+	/*wordCloud.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "기업별 사용 기술 스택 분석",
+		}),
+	)*/
+
+	data := make([]opts.WordCloudData, len(techstacks))
+	for i, techStack := range techstacks {
+		data[i] = opts.WordCloudData{
+			Name:  techStack.Name,
+			Value: techStack.Count,
+		}
+	}
+
+	return wordCloud
+}
+
 func GenerateGraph(topTechstacks []utils.TechstackCount, techstacks []utils.TechstackCount) {
 	categoryData := make(map[string][]utils.TechstackCount)
 	for _, techStack := range techstacks {
@@ -171,6 +191,7 @@ func GenerateGraph(topTechstacks []utils.TechstackCount, techstacks []utils.Tech
 
 	page := components.NewPage()
 	page.PageTitle = "기업별 사용 기술 스택 분석"
+	page.AddCharts(generateWordCloud(topTechstacks))
 	page.AddCharts(generateBar(topTechstacks))
 
 	for category, techStacks := range categoryData {
